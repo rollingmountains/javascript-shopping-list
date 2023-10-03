81fa0e6ac8c9609e124a5f44323aade8d4666484
rollingmountains <balakrishnapaudel@gmail.com>
2023-10-03 10:24:12 +0545

    clear UI and filter items

--- script.js
+++ script.js
@@ -7,6 +7,24 @@ const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 //4.clear all button
 const clrBtn = document.getElementById('clear');
+//5.filter item
+const filter = document.getElementById('filter');
+//6.container
+const container = document.querySelector('.container');
+
+//Function to clear elements
+function clearUI() {
+  const filter = document.querySelector('#filter');
+  filter.value = '';
+  const items = document.querySelectorAll('li');
+  if (items.length === 0) {
+    clrBtn.style.display = 'none';
+    filter.style.display = 'none';
+  } else {
+    clrBtn.style.display = 'block';
+    filter.style.display = 'block';
+  }
+}
 
 //Function to create icon
 function newIcon(iconClass) {
@@ -20,30 +38,34 @@ function newBtn(iconClass, btnClass) {
   btn.className = btnClass;
   //append icon element to button
   const icon = newIcon(iconClass);
-  btn.append(icon);
+  btn.appendChild(icon);
   return btn;
 }
 //Function to create new item list
 function newListItem() {
   const newListItem = document.createElement('li');
-  newListItem.append(document.createTextNode(itemInput.value));
+  newListItem.appendChild(document.createTextNode(itemInput.value));
   //append icon and btn element to new list item
   const iconButtonElement = newBtn(
     'fa-solid fa-xmark',
     'remove-item btn-link text-red'
   );
-  newListItem.append(iconButtonElement);
-  console.log(itemList);
+  newListItem.appendChild(iconButtonElement);
+  //console.log(itemList);
   //append new list item to the 'ul' list at the top
-  itemList.append(newListItem);
+  itemList.appendChild(newListItem);
 
   //clear the input field
   itemInput.value = '';
+  clearUI();
 }
+
+//Function to alert duplicate item
+
 //Function for onSubmit event
 function onSubmit(e) {
   e.preventDefault();
-  const newItemValue = itemInput.value;
+  const newItemValue = itemInput.value.toLowerCase();
   const btnClick = e.target.classList.contains('btn');
 
   if (newItemValue === '' && btnClick) {
@@ -58,26 +80,41 @@ function onClick(e) {
   if (e.target.classList.contains('fa-solid')) {
     const i = e.target.parentElement.parentElement;
     i.remove();
-    console.log(itemList);
   }
-  return;
+  clearUI();
 }
 
 //Function to clear all items from the list
 function onBtnClick() {
-  // const list = document.querySelectorAll('li');
-  // list.forEach((item) => {
-  //   item.remove();
-  // });
-  // itemList.innerHTML = '';
   while (itemList.firstChild) {
     itemList.removeChild(itemList.firstChild);
   }
+  clearUI();
+}
+
+//Function to filter items
+function onFilter(e) {
+  const li = document.querySelectorAll('li');
+  const input = e.target.value.toLowerCase();
+  // console.log(typeof input, input);
+
+  // console.log(li);
+  li.forEach((item) => {
+    // console.log(item.innerText.includes('App'));
+    if (item.innerText.toLowerCase().includes(input)) {
+      item.style.display = 'flex';
+    } else {
+      item.style.display = 'none';
+    }
+  });
 }
 
 //Add eventListener to elements
 form.addEventListener('click', onSubmit);
+filter.addEventListener('input', onFilter);
 itemList.addEventListener('click', onClick);
 clrBtn.addEventListener('click', onBtnClick);
 
+//Function to clear UI on page load
+clearUI();
 //
